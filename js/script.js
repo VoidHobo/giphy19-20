552d3112c83a562c3f4df023def981fee553f6fb
VoidHobo <alanhe.wifi@gmail.com>
2020-02-20 02:26:33 +0000

    did it commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 /* global $ */
-
-console.log('hi');
+//Create a fetch() request inside the click handler
+//Get the response from using your search URL              https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&    api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY
+ // Console log the response from your API call//
+//console.log('hi');
 
 $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
+  fetch("https://api.giphy.com/v1/gifs/search?q=puppy&     rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY")
+  .then(function(response){
+    response.json();
+  })
+  .then(function(data) {
+    console.log("Hello")
+  });
