fd06d1a0df5674b26dcd022cce054c3c4acccd63
VoidHobo <alanhe.wifi@gmail.com>
2020-03-05 02:08:32 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -8,10 +8,21 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
-  fetch("https://api.giphy.com/v1/gifs/search?q=puppy&     rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY")
-  .then(function(response){
-    response.json();
-  })
-  .then(function(data) {
-    console.log("Hello")
-  });
+
+$('button').click(function(){ 
+    var searchTerm = $("input").val();
+    
+    
+  var request_url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY";
+    
+  fetch(request_url)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(resp) {
+      var pic_url = resp.data;
+      //.images.downsized_large.url;
+      var index = Math.floor( Math.random() * pic_url.length );
+      $('body').append('<img src=' + pic_url + '>');
+    })
+});
